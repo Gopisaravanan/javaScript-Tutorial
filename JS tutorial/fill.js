af8e8e4e87326094a69827620963084230e18e99
Gopisaravanan.R <gopi.saravanan@kaaylabs.com>
2023-06-17 15:53:49 +0530

    added fill method

---
+++ JS tutorial/fill.js
@@ -0,0 +1,13 @@
+// fill method
+
+const temp = [2,3,4,1,2,3]
+
+temp.fill(7)
+
+console.log(temp)
+
+const fruits = ["apple", "mango", "oarange","banana"]
+
+const fillFruit = fruits.fill("kiwi")
+
+console.log(fillFruit)
